f55f6f90dc72a8625c315411687156c13a4b3329
Admir Heric <admir.heric@openresearch.com>
2019-12-11 14:47:12 +0100

    androidx migration

--- sample/src/main/java/com/github/clans/fab/sample/MainActivity.java
+++ sample/src/main/java/com/github/clans/fab/sample/MainActivity.java
@@ -3,7 +3,7 @@ package com.github.clans.fab.sample;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.support.v7.app.ActionBarActivity;
+import androidx.appcompat.app.ActionBarActivity;
 import android.view.View;
 import android.view.animation.AnimationUtils;
 import android.widget.AbsListView;
--- sample/src/main/java/com/github/clans/fab/sample/RecyclerViewActivity.java
+++ sample/src/main/java/com/github/clans/fab/sample/RecyclerViewActivity.java
@@ -2,9 +2,9 @@ package com.github.clans.fab.sample;
 
 import android.os.Bundle;
 import android.os.Handler;
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
+import androidx.appcompat.app.ActionBarActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
